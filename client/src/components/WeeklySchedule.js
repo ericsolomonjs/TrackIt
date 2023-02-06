0cfc6fc19ec437b96f5f7d677945bf4a615f95bc
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-02-06 18:24:58 -0500

    adding a tag for day cards

--- client/src/components/WeeklySchedule.js
+++ client/src/components/WeeklySchedule.js
@@ -8,39 +8,53 @@ export default function WeeklySchedule() {
     <br />
     <di className="caption-header"> Here's Your Weekly Workout Schedule!</di>
     <br />
+    <p className="info-note"> Click on a day to open up that day's workout plan</p>
+    <br/>
     <div className="main-container">
       <div className="row">
         <div className="column">
-          <div className="card">
-            <h3>Monday</h3>
-            <p>Biceps</p>
-          </div>
-
-          <div class="card">
-            <h3>Tuesday</h3>
-            <p>Back</p>
-          </div>
-
-          <div class="card">
-            <h3>Wednesday</h3>
-            <p>Chest</p>
-          </div>
-          <div class="card">
-            <h3>Thursday</h3>
-            <p>Cardio</p>
-          </div>
-          <div class="card">
-            <h3>Friday</h3>
-            <p>Rest</p>
-          </div>
-          <div class="card">
-            <h3>Saturday</h3>
-            <p>Legs</p>
-          </div>
-          <div class="card">
-            <h3>Sunday</h3>
-            <p>Rest</p>
-          </div>
+          <a className="card-click" href="/">
+            <div className="card">
+              <h3>Monday</h3>
+              <p>Biceps</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Tuesday</h3>
+              <p>Back</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Wednesday</h3>
+              <p>Chest</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Thursday</h3>
+              <p>Cardio</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Friday</h3>
+              <p>Rest</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Saturday</h3>
+              <p>Legs</p>
+            </div>
+          </a>
+          <a className="card-click" href="/">
+            <div class="card">
+              <h3>Sunday</h3>
+              <p>Rest</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
