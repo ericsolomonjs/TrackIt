d87f3b95405a805e95f1fecb16a26d7ec5502bfc
Scott Smyth <ssmythw@outlook.com>
2023-02-06 15:27:01 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import Signup from "./views/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Signup></Signup>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />}>
+            <Route index element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
