7c86864c36094d6312d8e536b274446e3b2742e2
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-07 12:02:32 -0500

    Apply bugfix for navbar styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Signup />}></Route>
