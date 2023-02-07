a49be1aa3868607ad366425ef2b4b5073edb3bcf
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-07 13:55:05 -0500

    add exercises routes

---
+++ backend/routes/exercises.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.get("/getByType/:type", async (req, res) => {
+  return json;
+});
+
+router.get("/", async (req, res) => {
+  return true;
+});
+
+module.exports = router;
