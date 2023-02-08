a45f1ee3d394e092baecd71942de30351a8936a5
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-07 19:55:08 -0500

    modify exercises schema

--- backend/db/schema/exercises.sql
+++ backend/db/schema/exercises.sql
@@ -1,10 +1,12 @@
--- Drop and recreate Users table
+-- Drop and recreate Exercises table
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+DROP TABLE IF EXISTS exercises CASCADE; 
+CREATE TABLE exercises (
+  id SERIAL NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  muscle VARCHAR(255) NOT NULL,
+  equipment VARCHAR(255) NOT NULL,
+  difficulty VARCHAR(255) NOT NULL,
+  instructions TEXT NOT NULL
 );
