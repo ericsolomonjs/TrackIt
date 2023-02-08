c713e03e9180d75acce7178376816f614c615b7b
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-02-08 14:26:18 -0500

    changed schedule img

--- frontend/src/components/SignedIn.js
+++ frontend/src/components/SignedIn.js
@@ -33,8 +33,9 @@ export default function SignedIn() {
         </div>
       </div>
       <div className="schedule-container">
-        <a href="/schedule" className="go-to-schedule"> Go to my full schedule </a>
-        <img className='my-schedule-image' src="https://t4.ftcdn.net/jpg/01/99/89/99/360_F_199899900_gy5JdpRH5iXzlVsyw08uCidWS4tpjVYF.jpg" />
+      <a href="/schedule" className="go-to-schedule"> Go to my full schedule </a>
+        <img className='my-schedule-image' src="https://images.unsplash.com/photo-1603077492579-39ff927823db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
+       
       </div>
     </div>
   </div>
