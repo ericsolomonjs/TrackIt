d16dda320d79db3ff21c8f724c1e3c6e5bc850c9
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-02-07 19:05:04 -0500

    fixing view port issue, added button styling

--- frontend/src/components/SignedIn.js
+++ frontend/src/components/SignedIn.js
@@ -3,26 +3,27 @@ import "../styles/SignedIn.css";
 
 
 export default function SignedIn() {
-  return <div className='main-container'>
+  return <div className='general-container'>
+    <br></br>
       <h3>Main Logged in </h3>
     <div className="my-workouts-grid">
       {/* add grid-container-view */}
       <div className="my-workouts-card">
         <img className="my-workouts-card-image" src="https://thumbs.dreamstime.com/b/highlighted-biceps-d-rendered-illustration-30721898.jpg" />
         <div className="workout-pic-info">
-          <a href="/workouts" className="my-arm-day"> My Arm Day</a>
+        <button className="btn-btn-primary"> My Arm Day</button>
         </div>
       </div>
       <div className="my-workouts-card">
         <img className="my-workouts-card-image" src="https://thumbs.dreamstime.com/b/highlighted-biceps-d-rendered-illustration-30721898.jpg" />
         <div className="workout-pic-info">
-          <a href="/workouts" className="my-arm-day"> My Arm Day</a>
+        <button className="btn-btn-primary"> My Arm Day</button>
         </div>
       </div>
       <div className="my-workouts-card">
         <img className="my-workouts-card-image" src="https://thumbs.dreamstime.com/b/highlighted-biceps-d-rendered-illustration-30721898.jpg" />
         <div className="workout-pic-info">
-          <a href="/workouts" className="my-arm-day"> My Arm Day</a>
+          <button className="btn-btn-primary"> My Arm Day</button>
         </div>
       </div>
     </div>
