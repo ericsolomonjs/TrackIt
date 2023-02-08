91c3ac0e918e3257a7bd325d4f691465c79bcdbf
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-08 12:53:59 -0500

    modify schedules routes

--- backend/routes/schedules.js
+++ backend/routes/schedules.js
@@ -1,18 +1,32 @@
 //schedules routes
 const router = require("express").Router();
+const { getSchedule, generateSchedule } = require("../db/queries/schedules");
 
 router.route("/")
+  //route for getting schedule
   .get(async (req, res) => {
-    //query to get the users schedule
-    const jsonObject = {}
-    //send object 
-    res.send(jsonObject)
+    const jsonObject = {};
+
+    if (req.user) {
+      try {
+        jsonObject = await getSchedule()
+        res.send(jsonObject)
+      }
+      catch (error) {
+        console.error(error)
+        res.sendStatus(404)
+      };
+    }
+    else {
+      res.sendStatus(418);
+    }
   })
 
-  .post(async (req, res) => {
+  //route for saving modified schedule
+  .put(async (req, res) => {
     let success = false;
     //generate the scheduleObj with API data
-    
+
     //query to save the users schedule
     if (req.user_id) {
       try { await SaveUserSchedule(req.user_id, req.scheduleObj) }
@@ -20,6 +34,13 @@ router.route("/")
       success = true;
     }
     res.send(success)
+  })
+
+router.route("/new/")
+  //route for generating a new schedule and returning it
+  .get(async (req, res) => {
+    const schedule = await generateSchedule(req.params);
+    res.send(schedule);
   })
 
 module.exports = router;
