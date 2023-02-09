48c47cf9e2f4090035a83505605fd0f5412e70c7
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-02-08 20:28:26 -0500

    fixed href path from schdule to days

--- frontend/src/components/WeeklySchedule.js
+++ frontend/src/components/WeeklySchedule.js
@@ -13,43 +13,43 @@ export default function WeeklySchedule() {
     <div className="main-container">
       <div className="row">
         <div className="column">
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div className="card">
               <h3>Monday</h3>
               <p>Biceps</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Tuesday</h3>
               <p>Back</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Wednesday</h3>
               <p>Chest</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Thursday</h3>
               <p>Cardio</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Friday</h3>
               <p>Rest</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Saturday</h3>
               <p>Legs</p>
             </div>
           </a>
-          <a className="card-click" href="/">
+          <a className="card-click" href="/days">
             <div class="card">
               <h3>Sunday</h3>
               <p>Rest</p>
