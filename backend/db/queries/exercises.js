a6187b55ed777a8a5a775e334eee4e0c40cbe53e
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-11 17:05:47 -0500

    modify exercises

--- backend/db/queries/exercises.js
+++ backend/db/queries/exercises.js
@@ -1,45 +1,62 @@
 const db = require("../connection");
 
 const getExercises = async () => {
-  const data = await db.query("SELECT * FROM exercises;")
+  const data = await db.query("SELECT * FROM exercises;");
   return Promise.resolve(data.rows);
 };
 
 const getExercisesForCardio = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE type=\"cardio\" && difficulty=\"$1\";", [difficulty]);
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE type='cardio' AND difficulty=$1;",
+    [difficulty]
+  );
   return Promise.resolve(data.rows);
 };
 
 const getExercisesForArms = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE muscle=\"biceps\" OR muscle=\"forearms\" OR muscle=\"triceps\" && difficulty=\"$1\";", [difficulty]);
-  return Promise.resolve(data.rows)
-}
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE difficulty= $1 AND (muscle='biceps' OR muscle='forearms' OR muscle='triceps');",
+    [difficulty]
+  );
+  return Promise.resolve(data.rows);
+};
 
 const getExercisesForLegs = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE muscle=\"calves\" OR muscle=\"glutes\" OR muscle=\"hamstrings\" && difficulty=\"$1\";", [difficulty]);
-  return Promise.resolve(data.rows)
-}
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='calves' OR muscle='glutes' OR muscle='hamstrings');",
+    [difficulty]
+  );
+  return Promise.resolve(data.rows);
+};
 
 const getExercisesForBack = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE muscle=\"lower_back\" OR muscle=\"middle_back\" OR muscle=\"traps\" && difficulty=\"$1\";", [difficulty]);
-  return Promise.resolve(data.rows)
-}
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='lower_back' OR muscle='middle_back' OR muscle='traps');",
+    [difficulty]
+  );
+  return Promise.resolve(data.rows);
+};
 
 const getExercisesForAbs = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE muscle=\"abdominals\" && difficulty=\"$1\";", [difficulty]);
-  return Promise.resolve(data.rows)
-}
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE difficulty=$1 AND muscle='abdominals';",
+    [difficulty]
+  );
+  return Promise.resolve(data.rows);
+};
 
 const getExercisesForChest = async (difficulty) => {
-  const data = await db.query("SELECT * FROM exercises WHERE muscle=\"chest\" && difficulty=\"$1\";", [difficulty]);
-  return Promise.resolve(data.rows)
-}
-
+  const data = await db.query(
+    "SELECT * FROM exercises WHERE muscle='chest' AND difficulty=$1;",
+    [difficulty]
+  );
+  return Promise.resolve(data.rows);
+};
 
 const clearExercises = async () => {
   const data = await db.query("DELETE FROM exercises;");
-  return Promise.resolve(data.rows)
-}
+  return Promise.resolve(data.rows);
+};
 
 const insertExercise = async (exerciseObj) => {
   const data = await db.query(
@@ -50,10 +67,10 @@ const insertExercise = async (exerciseObj) => {
       exerciseObj.muscle,
       exerciseObj.equipment,
       exerciseObj.difficulty,
-      exerciseObj.instructions
+      exerciseObj.instructions,
     ]
-  )
-  return Promise.resolve(data.rows)
+  );
+  return Promise.resolve(data.rows);
 };
 
 module.exports = {
@@ -65,5 +82,5 @@ module.exports = {
   getExercisesForLegs,
   getExercisesForBack,
   getExercisesForAbs,
-  getExercisesForChest
+  getExercisesForChest,
 };
