300bea62cb1e27244881aae018705a4d594921bd
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-11 14:35:02 -0500

    modify initDB

--- backend/db/initDB.js
+++ backend/db/initDB.js
@@ -4,35 +4,35 @@ const pg = require("pg");
 const fs = require("fs");
 
 const muscle_groups_sql = fs.readFileSync('db/schema/muscle_groups.sql', "utf8")
-  .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-  .replace(/\s+/g, ' ') // excess white space
-  .split(";") // split into all statements
+  .replace(/(\r\n|\n|\r)/gm, " ")
+  .replace(/\s+/g, ' ')
+  .split(";")
   .map(Function.prototype.call, String.prototype.trim)
   .filter(function (el) { return el.length != 0 });
 const schedules_sql = fs.readFileSync('db/schema/schedules.sql', "utf8")
-  .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-  .replace(/\s+/g, ' ') // excess white space
-  .split(";") // split into all statements
+  .replace(/(\r\n|\n|\r)/gm, " ")
+  .replace(/\s+/g, ' ')
+  .split(";")
   .map(Function.prototype.call, String.prototype.trim)
   .filter(function (el) { return el.length != 0 });
 const users_sql = fs.readFileSync('db/schema/users.sql', "utf8")
-  .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-  .replace(/\s+/g, ' ') // excess white space
-  .split(";") // split into all statements
+  .replace(/(\r\n|\n|\r)/gm, " ")
+  .replace(/\s+/g, ' ')
+  .split(";")
   .map(Function.prototype.call, String.prototype.trim)
   .filter(function (el) { return el.length != 0 });
 
 const users_seed = fs.readFileSync('db/seeds/users.sql', "utf8")
-  .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-  .replace(/\s+/g, ' ') // excess white space
-  .split(";") // split into all statements
+  .replace(/(\r\n|\n|\r)/gm, " ")
+  .replace(/\s+/g, ' ')
+  .split(";")
   .map(Function.prototype.call, String.prototype.trim)
   .filter(function (el) { return el.length != 0 });
 
 const exercises_sql = fs.readFileSync('db/schema/exercises.sql', "utf8")
-  .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
-  .replace(/\s+/g, ' ') // excess white space
-  .split(";") // split into all statements
+  .replace(/(\r\n|\n|\r)/gm, " ")
+  .replace(/\s+/g, ' ')
+  .split(";")
   .map(Function.prototype.call, String.prototype.trim)
   .filter(function (el) { return el.length != 0 });
 
@@ -61,6 +61,8 @@ async function runQueries() {
 }
 
 runQueries()
-  .then(() => { console.log("queries run successfully") })
+  .then(() => { console.log("queries run successfully") ;
+  process.exitCode = 1;
+})
 
 
