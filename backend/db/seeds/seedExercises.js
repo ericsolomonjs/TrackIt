36a914bf0b1276e316c5b683d9c45c6c5f40bcf2
Eric Solomon <esculpt@gmail.com>
2023-02-14 15:18:16 -0500

    add more exercises to init:exercises

--- backend/db/seeds/seedExercises.js
+++ backend/db/seeds/seedExercises.js
@@ -1,56 +1,61 @@
-const { insertExercise, getExercises, clearExercises } = require("../queries/exercises")
+const {
+  insertExercise,
+  getExercises,
+  clearExercises,
+} = require("../queries/exercises");
 require("dotenv").config();
 const Axios = require("axios");
 
 async function saveExerciseBatch(exercises) {
-  for (let exercise of exercises) {
-    await insertExercise(exercise)
+  try {for (let exercise of exercises) {
+    await insertExercise(exercise);
   }
-  return Promise.resolve();
+  return;} catch (e) {return e;}
 }
 
 async function initExercises() {
-  const muscleGroupsArr = [
-    "abdominals",
-    "abductors",
-    "adductors",
-    "biceps",
-    "calves",
-    "chest",
-    "forearms",
-    "glutes",
-    "hamstrings",
-    "lats",
-    "lower_back",
-    "middle_back",
-    "neck",
-    "quadriceps",
-    "traps",
-    "triceps",
-  ]
+  // const muscleGroupsArr = [
+  //   "abdominals",
+  //   "abductors",
+  //   "adductors",
+  //   "biceps",
+  //   "calves",
+  //   "chest",
+  //   "forearms",
+  //   "glutes",
+  //   "hamstrings",
+  //   "lats",
+  //   "lower_back",
+  //   "middle_back",
+  //   "neck",
+  //   "quadriceps",
+  //   "traps",
+  //   "triceps",
+  // ];
 
-  for (let muscleGroup of muscleGroupsArr) {
-    console.log("muscleGroup = : ", muscleGroup)
-    const moreExercises = await Axios.get(`https://api.api-ninjas.com/v1/exercises?muscle=${muscleGroup}`, { headers: { "X-Api-Key": process.env.ENVApiKey } })  
+  // for (let muscleGroup of muscleGroupsArr) {
+  //   console.log("muscleGroup = : ", muscleGroup)
+  //   const moreExercises = await Axios.get(`https://api.api-ninjas.com/v1/exercises?muscle=${muscleGroup}`, { headers: { "X-Api-Key": process.env.ENVApiKey } })
+  //   await saveExerciseBatch(moreExercises.data);
+  // }
+  try {for (let i = 0; i < 5000; i = i + 10) {
+    console.log("i = : ", i);
+    const moreExercises = await Axios.get(
+      `https://api.api-ninjas.com/v1/exercises?offset=${i}`,
+      { headers: { "X-Api-Key": process.env.ENVApiKey } }
+    );
     await saveExerciseBatch(moreExercises.data);
   }
-  // const cardioExercises = await Axios.get(`https://api.api-ninjas.com/v1/exercises?type=cardio`, { headers: { "X-Api-Key": process.env.ENVApiKey } })
-  // await saveExerciseBatch(cardioExercises.data);
-
-  return Promise.resolve()
-};
+  return;} catch (e) {return e;}
+}
 
 clearExercises();
-initExercises()
-  .then(() => {
-    getExercises()
-      .then((data) => {
-        console.log(data.length + " exercise rows created");
-        console.log(data)
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-  })
-
-
+initExercises().then(() => {
+  getExercises()
+    .then((data) => {
+      console.log(data.length + " exercise rows created");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
