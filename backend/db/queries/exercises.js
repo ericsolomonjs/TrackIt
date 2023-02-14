b1779190c04bf2c019a67602d0ff11632debc165
Eric Solomon <eric.solomon.ca@gmail.com>
2023-02-13 19:56:45 -0500

    fix double promises in queries

--- backend/db/queries/exercises.js
+++ backend/db/queries/exercises.js
@@ -6,71 +6,103 @@ const getExercises = async () => {
 };
 
 const getExercisesForCardio = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE type='cardio' AND difficulty=$1;",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE type='cardio' AND difficulty=$1;",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getExercisesForArms = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE difficulty= $1 AND (muscle='biceps' OR muscle='forearms' OR muscle='triceps');",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE difficulty= $1 AND (muscle='biceps' OR muscle='forearms' OR muscle='triceps');",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getExercisesForLegs = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='calves' OR muscle='glutes' OR muscle='hamstrings');",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='calves' OR muscle='glutes' OR muscle='hamstrings');",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getExercisesForBack = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='lower_back' OR muscle='middle_back' OR muscle='traps');",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE difficulty=$1 AND (muscle='lower_back' OR muscle='middle_back' OR muscle='traps');",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getExercisesForAbs = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE difficulty=$1 AND muscle='abdominals';",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE difficulty=$1 AND muscle='abdominals';",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const getExercisesForChest = async (difficulty) => {
-  const data = await db.query(
-    "SELECT * FROM exercises WHERE muscle='chest' AND difficulty=$1;",
-    [difficulty]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT * FROM exercises WHERE muscle='chest' AND difficulty=$1;",
+      [difficulty]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const clearExercises = async () => {
-  const data = await db.query("DELETE FROM exercises;");
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query("DELETE FROM exercises;");
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 const insertExercise = async (exerciseObj) => {
-  const data = await db.query(
-    "INSERT INTO exercises(name, type, muscle, equipment, difficulty, instructions) VALUES ($1, $2, $3, $4, $5, $6);",
-    [
-      exerciseObj.name,
-      exerciseObj.type,
-      exerciseObj.muscle,
-      exerciseObj.equipment,
-      exerciseObj.difficulty,
-      exerciseObj.instructions,
-    ]
-  );
-  return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "INSERT INTO exercises(name, type, muscle, equipment, difficulty, instructions) VALUES ($1, $2, $3, $4, $5, $6);",
+      [
+        exerciseObj.name,
+        exerciseObj.type,
+        exerciseObj.muscle,
+        exerciseObj.equipment,
+        exerciseObj.difficulty,
+        exerciseObj.instructions,
+      ]
+    );
+    return data.rows;
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = {
--- backend/db/queries/schedules.js
+++ backend/db/queries/schedules.js
@@ -8,7 +8,7 @@ const {
   getExercisesForChest,
 } = require("./exercises");
 
-const shuffleArray = async (array) => {
+const shuffleArray = (array) => {
   let index = array.length;
   let randomIndex;
   while (index != 0) {
@@ -16,154 +16,172 @@ const shuffleArray = async (array) => {
     index--;
     [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
   }
-  return Promise.resolve(array);
+  return array;
 };
 
 const getSchedule = async (userId) => {
-  const data = await db.query(
-    "SELECT schedule FROM schedules WHERE user_id = $1 LIMIT 1;",
-    [userId]
-  );
-  if (data.rows.length > 0) {
-    return Promise.resolve(data.rows);
+  try {
+    const data = await db.query(
+      "SELECT schedule FROM schedules WHERE user_id = $1 LIMIT 1;",
+      [userId]
+    );
+    if (data.rows.length > 0) {
+      return Promise.resolve(data.rows);
+    }
+  } catch (error) {
+    return error;
   }
-  return Promise.reject();
 };
 
 const deleteSchedule = async (userId) => {
-  await db.query("DELETE FROM schedules WHERE user_id = $1;", [user_id]);
-  return Promise.resolve();
+  try {
+    await db.query("DELETE FROM schedules WHERE user_id = $1;", [user_id]);
+    return;
+  } catch (error) {
+    return error;
+  }
 };
 
 const saveSchedule = async (schedule, user_id) => {
-  await db.query("INSERT INTO schedules (schedule, user_id) VALUES ($1,$2);", [
-    schedule,
-    user_id,
-  ]);
-  return Promise.resolve();
+  try {
+    await db.query(
+      "INSERT INTO schedules (schedule, user_id) VALUES ($1,$2);",
+      [schedule, user_id]
+    );
+    return;
+  } catch (error) {
+    return error;
+  }
 };
 
 const generateSchedule = async (params) => {
-  console.log("params: ", params);
-  const difficulty = params.difficulty;
-  const scheduleObj = {
-    time: params.time,
-    difficulty: difficulty,
-    0: {
-      exercises: [],
-      daysFocus: "",
-    },
-    1: {
-      exercises: [],
-      daysFocus: "",
-    },
-    2: {
-      exercises: [],
-      daysFocus: "",
-    },
-    3: {
-      exercises: [],
-      daysFocus: "",
-    },
-    4: {
-      exercises: [],
-      daysFocus: "",
-    },
-    5: {
-      exercises: [],
-      daysFocus: "rest",
-    },
-    6: {
-      exercises: [],
-      daysFocus: "rest",
-    },
-  };
+  try {
+    console.log("params: ", params);
+    const difficulty = params.difficulty;
+    const scheduleObj = {
+      time: params.time,
+      difficulty: difficulty,
+      0: {
+        exercises: [],
+        daysFocus: "",
+      },
+      1: {
+        exercises: [],
+        daysFocus: "",
+      },
+      2: {
+        exercises: [],
+        daysFocus: "",
+      },
+      3: {
+        exercises: [],
+        daysFocus: "",
+      },
+      4: {
+        exercises: [],
+        daysFocus: "",
+      },
+      5: {
+        exercises: [],
+        daysFocus: "rest",
+      },
+      6: {
+        exercises: [],
+        daysFocus: "rest",
+      },
+    };
 
-  while (
-    scheduleObj["0"].daysFocus === "" ||
-    scheduleObj["1"].daysFocus === "" ||
-    scheduleObj["2"].daysFocus === "" ||
-    scheduleObj["3"].daysFocus === "" ||
-    scheduleObj["4"].daysFocus === "" ||
-    scheduleObj["5"].daysFocus === "" ||
-    scheduleObj["6"].daysFocus === ""
-  ) {
-    if (params.arms) {
-      for (let i = 0; i < 7; i++) {
-        if (scheduleObj[`${i}`].daysFocus === "") {
-          exercises = await getExercisesForArms(difficulty);
-          for (let exercise of exercises)
-            scheduleObj[`${i}`].exercises.push(exercise);
-          scheduleObj[`${i}`].daysFocus = "arms";
-          break;
+    while (
+      scheduleObj["0"].daysFocus === "" ||
+      scheduleObj["1"].daysFocus === "" ||
+      scheduleObj["2"].daysFocus === "" ||
+      scheduleObj["3"].daysFocus === "" ||
+      scheduleObj["4"].daysFocus === "" ||
+      scheduleObj["5"].daysFocus === "" ||
+      scheduleObj["6"].daysFocus === ""
+    ) {
+      if (params.arms) {
+        for (let i = 0; i < 7; i++) {
+          if (scheduleObj[`${i}`].daysFocus === "") {
+            exercises = await getExercisesForArms(difficulty);
+            for (let exercise of exercises)
+              scheduleObj[`${i}`].exercises.push(exercise);
+            scheduleObj[`${i}`].daysFocus = "arms";
+            break;
+          }
+        }
+      }
+
+      if (params.legs) {
+        for (let i = 0; i < 7; i++) {
+          if (scheduleObj[`${i}`].daysFocus === "") {
+            exercises = await getExercisesForLegs(difficulty);
+            for (let exercise of exercises)
+              scheduleObj[`${i}`].exercises.push(exercise);
+            scheduleObj[`${i}`].daysFocus = "legs";
+            break;
+          }
+        }
+      }
+      if (params.chest) {
+        for (let i = 0; i < 7; i++) {
+          if (scheduleObj[`${i}`].daysFocus === "") {
+            exercises = await getExercisesForChest(difficulty);
+            for (let exercise of exercises)
+              scheduleObj[`${i}`].exercises.push(exercise);
+            scheduleObj[`${i}`].daysFocus = "chest";
+            break;
+          }
+        }
+      }
+      if (params.abs) {
+        for (let i = 0; i < 7; i++) {
+          if (scheduleObj[`${i}`].daysFocus === "") {
+            exercises = await getExercisesForAbs(difficulty);
+            for (let exercise of exercises)
+              scheduleObj[`${i}`].exercises.push(exercise);
+            scheduleObj[`${i}`].daysFocus = "abs";
+            break;
+          }
+        }
+      }
+      if (params.back) {
+        for (let i = 0; i < 7; i++) {
+          if (scheduleObj[`${i}`].daysFocus === "") {
+            exercises = await getExercisesForBack(difficulty);
+            for (let exercise of exercises)
+              scheduleObj[`${i}`].exercises.push(exercise);
+            scheduleObj[`${i}`].daysFocus = "back";
+            break;
+          }
         }
       }
     }
 
-    if (params.legs) {
-      for (let i = 0; i < 7; i++) {
-        if (scheduleObj[`${i}`].daysFocus === "") {
-          exercises = await getExercisesForLegs(difficulty);
-          for (let exercise of exercises)
-            scheduleObj[`${i}`].exercises.push(exercise);
-          scheduleObj[`${i}`].daysFocus = "legs";
-          break;
-        }
-      }
+    //shuffle the exercises
+    for (let i = 0; i < 7; i++) {
+      scheduleObj[`${i}`].exercises = await shuffleArray(
+        scheduleObj[`${i}`].exercises
+      );
     }
-    if (params.chest) {
-      for (let i = 0; i < 7; i++) {
-        if (scheduleObj[`${i}`].daysFocus === "") {
-          exercises = await getExercisesForChest(difficulty);
-          for (let exercise of exercises)
-            scheduleObj[`${i}`].exercises.push(exercise);
-          scheduleObj[`${i}`].daysFocus = "chest";
-          break;
-        }
-      }
+    //truncate the exercises for time
+    //truncate fn
+    for (let i = 0; i < 7; i++) {
+      scheduleObj[`${i}`].exercises = scheduleObj[`${i}`].exercises.slice(
+        0,
+        10
+      ); //10 items for a 15 minutes schedule incl rest breaks
     }
-    if (params.abs) {
-      for (let i = 0; i < 7; i++) {
-        if (scheduleObj[`${i}`].daysFocus === "") {
-          exercises = await getExercisesForAbs(difficulty);
-          for (let exercise of exercises)
-            scheduleObj[`${i}`].exercises.push(exercise);
-          scheduleObj[`${i}`].daysFocus = "abs";
-          break;
-        }
-      }
-    }
-    if (params.back) {
-      for (let i = 0; i < 7; i++) {
-        if (scheduleObj[`${i}`].daysFocus === "") {
-          exercises = await getExercisesForBack(difficulty);
-          for (let exercise of exercises)
-            scheduleObj[`${i}`].exercises.push(exercise);
-          scheduleObj[`${i}`].daysFocus = "back";
-          break;
-        }
-      }
-    }
+    //return promise with scheduleObj completed
+    return scheduleObj;
+  } catch (error) {
+    return error;
   }
-
-  //shuffle the exercises
-  for (let i = 0; i < 7; i++) {
-    scheduleObj[`${i}`].exercises = await shuffleArray(
-      scheduleObj[`${i}`].exercises
-    );
-  }
-  //truncate the exercises for time
-  //truncate fn
-  for (let i = 0; i < 7; i++) {
-    scheduleObj[`${i}`].exercises = scheduleObj[`${i}`].exercises.slice(0, 10); //10 items for a 15 minutes schedule incl rest breaks
-  }
-  //return promise with scheduleObj completed
-  return Promise.resolve(scheduleObj);
 };
 
 module.exports = {
   getSchedule,
   generateSchedule,
   saveSchedule,
-  deleteSchedule
+  deleteSchedule,
 };
