fca9a1625e6d78bff6afaed20101315652b462cb
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-02-15 16:02:03 -0500

    home page changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Home.css";
 import "../styles/main.css";
+import Card from "react-bootstrap/Card";
 
 export default function Home() {
   return (
@@ -8,31 +9,32 @@ export default function Home() {
       <section>
         <div className="section1-container">
           <img className="home-img2" src="workout2.jpg" alt="" />
-          <p
-            className="info-textbox"
-            style={{ fontSize: "1.75rem", textAlign: "center" }}
-          >
-            Track it is a web application that allows you to input the muscles
-            you want to workout, while we handle finding you the exercises that
-            can do that. Taking the stress off the user so that they can focus
-            on working out and Track it takes care of the rest.
-          </p>
+          <Card className="home-info-card">
+            <Card.Title className="info-card-title">What is it?</Card.Title>
+            <br />
+            <Card.Subtitle className="info-card-info">Track it is a web application that allows you to input the specific muscles you want to workout, while we handle finding you the exercises that can do that.</Card.Subtitle>
+          </Card>
         </div>
       </section>
       <section>
         <div className="section2-container">
-          <p
-            className="info-textbox"
-            style={{ fontSize: "1.75rem", textAlign: "center" }}
-          >
-            Built to help target major muscle groups including chest, arms,
-            legs, back, etc. We have an exercise pool that ranges from beginner
-            level up towards advanced and is constantly expanding, meaning there
-            is something for everyone no matter your experience level!
-          </p>
+        <Card className="home-info-card">
+            <Card.Title className="info-card-title">Who's it for?</Card.Title>
+            <br />
+            <Card.Subtitle className="info-card-info"> Everyone! We have an exercise pool that ranges from beginner level up towards advanced and is constantly expanding, meaning there is something for everyone no matter your experience level!</Card.Subtitle>
+          </Card>
           <img className="home-img2" src="workout3.jpg" alt="" />
         </div>
       </section>
+      <section>
+        <br />
+      <Card className="home-info-card-two">
+            <Card.Title className="info-card-title">About Us</Card.Title>
+            <br />
+            <Card.Subtitle className="info-card-about">We're just 3 guys that love to workout. Everyone should workout and we knew that the surplus of info out there deters people from even thinking about starting! We realized that saving the time for people to figure out what to do can go a long way and thats just what we've done here. Laziness is the enermy of success, and we just want to help you win. </Card.Subtitle>
+          </Card>
+      </section>
+      <br />
       <section>
         <div className="footer-img-one">
           <img className="motiv-img" src="do-great.png" alt="" />
